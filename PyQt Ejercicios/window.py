7ebf4ad4174602e27d0b5eb8e395369a243c8535
MARSA <salvatierrabellido@gmail.com>
2023-11-04 17:39:05 +0100

    Add window, first commit of PyQt5 course

--- PyQt Ejercicios/window.py
+++ PyQt Ejercicios/window.py
@@ -1,1 +1,20 @@
-# this is my first PyQt5 exercise
+import sys
+import typing
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import *
+
+
+class Window(QWidget):
+    def __init__(self, ):
+        super().__init__()
+        self.setGeometry(50,50,300,450) # location of my window (x,y) and size of my window (x,y)
+        self.setWindowTitle("This is our window title")
+
+        self.show()
+
+
+App= QApplication(sys.argv)
+window=Window()
+sys.exit(App.exec_())
+
+
