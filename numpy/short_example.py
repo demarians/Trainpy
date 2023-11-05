9b16264fa52d025d533be441d13b262a23f8b12c
MARSA <salvatierrabellido@gmail.com>
2023-11-05 13:18:00 +0100

    Add numpy first example

--- numpy/short_example.py
+++ numpy/short_example.py
@@ -1,1 +1,7 @@
 # quick example how to treat numpy arrays
+import numpy as np
+
+
+prueba=np.array([2,3,4,5])
+
+print(prueba)
--- test_con_NUMPY.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-prueba=np.array([2,3,4,5])
-
-print(prueba)
-
