32cde14195527b99fad9e5da9d193fee585eae7c
MARSA <salvatierrabellido@gmail.com>
2023-11-06 21:03:32 +0100

    Update game, timer set

--- Projects/first_proj/rock_per_scisors.py
+++ Projects/first_proj/rock_per_scisors.py
@@ -2,6 +2,8 @@ import sys
 import typing
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont,QPixmap
+from PyQt5.QtCore import QTimer
+from random import randint
 
 textFont=QFont("Times",14)
 buttonFont=QFont("Arial",12)
@@ -33,10 +35,43 @@ class Window(QWidget):
        self.imageGame=QLabel(self)
        self.imageGame.setPixmap(QPixmap("C:/Users/salva/Repositories/Trainpy/Projects/first_proj/images/game.png"))
        self.imageGame.move(230,160)
+       #############Buttons################
+       btnStart=QPushButton("Start",self)
+       btnStop=QPushButton("Stop",self)
+       btnStart.move(130,250)
+       btnStop.move(280, 250)
+       btnStart.clicked.connect(self.start)
+       btnStop.clicked.connect(self.stop)
+       self.timer=QTimer(self)
+       self.timer.setInterval(100)
+       self.timer.timeout.connect(self.playGame)
 
-    
+        
        self.show()
 
+    
+    def playGame(self):
+        self.rndComputer=randint(1,3)
+        if self.rndComputer == 1:
+            self.imageComputer.setPixmap(QPixmap("C:/Users/salva/Repositories/Trainpy/Projects/first_proj/images/rock.png"))
+        elif self.rndComputer == 2:
+            self.imageComputer.setPixmap(QPixmap("C:/Users/salva/Repositories/Trainpy/Projects/first_proj/images/paper.png"))
+        else:
+            self.imageComputer.setPixmap(QPixmap("C:/Users/salva/Repositories/Trainpy/Projects/first_proj/images/scissors.png"))
+        self.rndPlayer=randint(1,3)
+        if self.rndPlayer == 1:
+            self.imagePlayer.setPixmap(QPixmap("C:/Users/salva/Repositories/Trainpy/Projects/first_proj/images/rock.png"))
+        elif self.rndPlayer == 2:
+            self.imagePlayer.setPixmap(QPixmap("C:/Users/salva/Repositories/Trainpy/Projects/first_proj/images/paper.png"))
+        else:
+            self.imagePlayer.setPixmap(QPixmap("C:/Users/salva/Repositories/Trainpy/Projects/first_proj/images/scissors.png"))
+
+
+    def start(self):
+        self.timer.start()
+
+    def stop(self):
+        self.timer.stop()
 
 
     
